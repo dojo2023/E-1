6daf0cc8540c7729a67797083b7c34ed19b4e5fd
梅原みぎわ <umeharamigiwaplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-15 15:00:10 +0900

    謎エラー

--- src/servlet/FAQServlet.java
+++ src/servlet/FAQServlet.java
@@ -1,28 +1,33 @@
 package servlet;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-public class FAQServlet extends HttpServlet{
+/**
+ * Servlet implementation class FAQServlet
+ */
+@WebServlet("/FAQServlet")
+public class FAQServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-		HttpSession session = request.getSession();
-
-/*		if (session.getAttribute("id") == null) {
-			response.sendRedirect("/AllNightCalender/LoginServlet");
-			return;
-		}
-*/
-
-		// よくある質問ページにフォワードする
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/FAQ.jsp");
-		dispatcher.forward(request, response);
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
 }
