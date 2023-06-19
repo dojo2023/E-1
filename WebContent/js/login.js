c5e2930dbdbb2b1c16f02d59eb8722101033ec52
dojo2023-kubo <130334539+dojo2023-kubo@users.noreply.github.com>
2023-06-19 14:59:38 +0900

    変更

--- WebContent/js/login.js
+++ WebContent/js/login.js
@@ -2,13 +2,29 @@
  *
  */
 	'use strict';
-	document.getElementById('sendBtn').onclick = function (e) {
+
+    function resetContent () {
+    	const id = document.getElementById('userID');
+		const pass = document.getElementById('userPW');
+
+    	id.placeholder = "ID";
+    	pass.placeholder = "PW";
+    }
+
+	document.getElementById("sendBtn").onclick = function (e) {
 		e.preventDefault();
 		const id = document.getElementById('userID');
 		const pass = document.getElementById('userPW');
-		if(!id.value || !pass.value){
-			document.getElementById('error').textContent = "※IDとパスワードを入力してください";
-		} else {
+
+		if(!id.value){
+			id.placeholder = "※IDを入力してください";
+		}
+
+		if(!pass.value){
+			pass.placeholder = "※パスワードを入力してください";
+		}
+
+		if(id.value && pass.value) {
 			document.myform.submit();
 		}
 	}
