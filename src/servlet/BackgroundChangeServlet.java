6896f284656107d7a3561434066d3019d26af28b
dojo2023-hiramatsu <130335025+dojo2023-hiramatsu@users.noreply.github.com>
2023-06-22 17:59:06 +0900

    ポイント詳細画面変更

--- src/servlet/BackgroundChangeServlet.java
+++ src/servlet/BackgroundChangeServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +10,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.Rank_bonusDAO;
+import model.Rank_bonus;
 
 /**
  * Servlet implementation class BackgroundChangeServlet
@@ -24,17 +29,47 @@ public class BackgroundChangeServlet extends HttpServlet {
 		response.setContentType("text/html;charset=UTF-8");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 
+		Calendar calender = Calendar.getInstance();
+
+		int y = calender.get(calender.YEAR)+1;
+		int m = calender.get(calender.MONTH)+1;
+
+		Rank_bonus bonus = new Rank_bonus();
+
+		//現在の月と年を入れる
+		bonus.setYear(y+"");
+		bonus.setMonth(m+"");
+
+
+
+		Rank_bonusDAO bDao = new Rank_bonusDAO();
+		List<Rank_bonus> bonusList = bDao.select(bonus);
+
+
+
+		request.setAttribute("j",bonusList);
+
 		// メニューページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/backgroundChange.jsp");
+
 		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+
+		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			String url = request.getParameter("url");
+
+			request.setAttribute("url", url);
+
+			// ページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/backgroundChange.jsp");
+			dispatcher.forward(request, response);
+
+
 	}
 
 }
