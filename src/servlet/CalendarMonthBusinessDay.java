a4ee2f80c07788c2601957c56ea9883822f1b0c7
dojo2023-matsumoto-y <130334568+dojo2023-matsumoto-y@users.noreply.github.com>
2023-06-28 15:26:54 +0900

    手直し

--- src/servlet/CalendarMonthBusinessDay.java
+++ src/servlet/CalendarMonthBusinessDay.java
@@ -39,28 +39,28 @@ public class CalendarMonthBusinessDay extends HttpServlet {
 		session.setAttribute("day", day);
 		session.setAttribute("planDayList", planDayList);
 
-		if (x.equals("business") && y.equals("0") && z.equals("0")) {
+		if (x.equals("business") && y.equals("0")) {
 			List<Plan> planList = pDao.look((String)session.getAttribute("id"),x);
 			session.setAttribute("planList", planList);
 
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/CalendarMonthBusinessDay.jsp");
 			dispatcher.forward(request, response);
 		}
-		else if (x.equals("business") && y.equals("1") && z.equals("0")) {
+		else if (x.equals("business") && y.equals("1")) {
 			List<Plan> planList = pDao.look((String)session.getAttribute("id"),x);
 			session.setAttribute("planList", planList);
 
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/CalendarMonthBusinessTetsuyaModeTimeOffDay.jsp");
 			dispatcher.forward(request, response);
 		}
-		else if (x.equals("private") && y.equals("0") && z.equals("0")) {
+		else if (x.equals("private") && y.equals("0")) {
 			List<Plan> planList = pDao.look((String)session.getAttribute("id"),x);
 			session.setAttribute("planList", planList);
 
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/CalendarMonthPrivateDay.jsp");
 			dispatcher.forward(request, response);
 		}
-		else if (x.equals("private") && y.equals("1") && z.equals("0")) {
+		else if (x.equals("private") && y.equals("1")) {
 			List<Plan> planList = pDao.look((String)session.getAttribute("id"),x);
 			session.setAttribute("planList", planList);
 
--- src/servlet/CalendarServlet.java
+++ src/servlet/CalendarServlet.java
@@ -35,28 +35,28 @@ public class CalendarServlet extends HttpServlet {
 
 		PlanDAO pDao = new PlanDAO();
 
-		if (x.equals("business") && y.equals("0") && z.equals("0")) {
+		if (x.equals("business") && y.equals("0")) {
 			List<Plan> planList = pDao.look((String)session.getAttribute("id"),x);
 			session.setAttribute("planList", planList);
 
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/CalendarMonthBusiness.jsp");
 			dispatcher.forward(request, response);
 		}
-		else if (x.equals("business") && y.equals("1") && z.equals("0")) {
+		else if (x.equals("business") && y.equals("1")) {
 			List<Plan> planList = pDao.look((String)session.getAttribute("id"),x);
 			session.setAttribute("planList", planList);
 
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/CalendarMonthBusinessTetsuyaModeTimeOff.jsp");
 			dispatcher.forward(request, response);
 		}
-		else if (x.equals("private") && y.equals("0") && z.equals("0")) {
+		else if (x.equals("private") && y.equals("0")) {
 			List<Plan> planList = pDao.look((String)session.getAttribute("id"),x);
 			session.setAttribute("planList", planList);
 
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/CalendarMonthPrivate.jsp");
 			dispatcher.forward(request, response);
 		}
-		else if (x.equals("private") && y.equals("1") && z.equals("0")) {
+		else if (x.equals("private") && y.equals("1")) {
 			List<Plan> planList = pDao.look((String)session.getAttribute("id"),x);
 			session.setAttribute("planList", planList);
 
